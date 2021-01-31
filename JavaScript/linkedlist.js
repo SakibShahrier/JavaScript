dae408ebc4df7e4f7a9d7a0702aee34e0c209c8b
SakibShahrier <71575450+SakibShahrier@users.noreply.github.com>
2021-02-01 04:05:06 +0600

    first commit

---
+++ JavaScript/linkedlist.js
@@ -0,0 +1,35 @@
+let temp, prev1;
+let head = {
+  prev : null,
+  value : 0,
+  next : null
+}
+
+for(let i = 1; i <= 10; ++i){
+
+  if(head.next === null){
+    temp = {
+      prev : head,
+      value : i,
+      next : null
+    }
+    temp.next = temp;
+    head.next = temp;
+    prev1 = temp;
+  }else{
+    temp = {
+      prev : prev1,
+      value : i,
+      next : null
+    }
+    prev1.next = temp;
+    prev1 = temp;
+  }
+}
+
+temp = head;
+
+for( ;temp.next !== null; ){
+  console.log(temp.value);
+  temp = temp.next;
+}
